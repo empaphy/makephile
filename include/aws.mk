56809fbaa54407594924fab0e1f700792a96383b
Alwin Garside <alwin@myparcel.nl>
2023-07-27 11:41:23 +0200

    fix: make makephile_aws stuff "public"

--- include/aws.mk
+++ include/aws.mk
@@ -2,23 +2,23 @@
 # AWS functions for Makephile.
 ########################################
 
-AWS_ACCOUNT           ?= $(shell $(philmk_aws) sts get-caller-identity | jq --raw-output --exit-status '.Account')
-AWS_ACCESS_KEY_ID     ?= $(shell $(philmk_aws) configure get aws_access_key_id)
-AWS_SECRET_ACCESS_KEY ?= $(shell $(philmk_aws) configure get aws_secret_access_key)
-AWS_REGION            ?= $(shell $(philmk_aws) configure get region)
+AWS_ACCOUNT           ?= $(shell $(makephile_aws) sts get-caller-identity | jq --raw-output --exit-status '.Account')
+AWS_ACCESS_KEY_ID     ?= $(shell $(makephile_aws) configure get aws_access_key_id)
+AWS_SECRET_ACCESS_KEY ?= $(shell $(makephile_aws) configure get aws_secret_access_key)
+AWS_REGION            ?= $(shell $(makephile_aws) configure get region)
 
 ifndef AWS_PROFILE
-	philmk_aws_profile_option =
+	makephile_aws_profile_option =
 else ifeq ($(AWS_PROFILE), default)
-	philmk_aws_profile_option =
+	makephile_aws_profile_option =
 else
-	philmk_aws_profile_option = --profile '$(AWS_PROFILE)'
+	makephile_aws_profile_option = --profile '$(AWS_PROFILE)'
 
 	export AWS_ACCOUNT
 	export AWS_REGION
 endif
 
-philmk_aws = aws $(philmk_aws_profile_option)
+makephile_aws = aws $(makephile_aws_profile_option)
 
 ##
 # Exports the AWS credentials & related environment variables.
